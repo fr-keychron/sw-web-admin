464455a399f3b77dc066cef4011022132221e1e2
sirius.black <siriusblack@keychron.cn>
2024-01-08 16:16:40 +0800

    Feat

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,10 +1,9 @@
 // const host = 'http://localhost:4001/'
 const host = 'http://47.254.44.78:4001/'
-
 const gap = 2
 export const GLOBAL_CONFIG = {
 	HOST: host + "api/",
-	STATIC: host,
+	STATIC:host + "api/",
 	KeyboardLayout: {
 		width: 46 + gap,
 		height: 48 + gap,
